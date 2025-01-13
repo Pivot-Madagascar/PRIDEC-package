d6450b0604457b3ac5f5586171248f288c73f687
mv_evans <mv.evans.phd@gmail.com>
2025-01-13 16:47:13 +0200

    tests for prep_caseData

--- notebook_pridec_pkg.md
+++ notebook_pridec_pkg.md
@@ -2,6 +2,8 @@
 
 ## Development workflow
 
+*All use the devtools package*
+
 `use_r()`: creates an R script with that filename to hold the function
 `use_test()`: creates an R script to hold the tests for that function
 `use_package()`: adds package to `Import` in `DESCRIPTION`. call functions using `package::function`
@@ -17,6 +19,26 @@
 
 For more info, use [R packages book](https://r-pkgs.org/).
 
+## 2024-01-13
+
+Working on CV split functions. I've got these written with some tests, but they aren't anything crazy.
+
+Also wrote tests for the prep_caseData functions.
+
+Started writing documents for the demo data, but having issues with the `check` picking it up. 
+
+**TO DO:**
+- ~~write tests for seasonal_fill~~
+- ~~write more tests for prep_caseData~~
+- document demo data (demo_malaria, demo_polygon)
+- start a naive modeling function
+
 ## 2024-01-08
 
-Started development in package architecture. This involves moving things over from the modeling repo.
+Started development in package architecture. This involves moving things over from the modeling repo. One thing I really needed to be able to do this was a demo dataset to test things on. I created a simulated dataset of malaria cases from some data in Ifanadiana, `demo_malaria`, and the spatial polygon that goes with it, `demo_polygon`. This should work for testing everything out (hopefully).
+
+I have worked through the data_prep step, now I need to do the cv_split steps to finish all the data treatment
+
+**TO DO:**
+- cv split functions
+- start modeling functions (start with naive and glm?)
--- tests/testthat/test-prep_caseData.R
+++ tests/testthat/test-prep_caseData.R
@@ -1,18 +1,35 @@
 test_that("data preparation function works", {
   data(demo_malaria)
   data(demo_polygon)
-  lagged_vars = c("pev", "rain_mm", "temp_c")
-  scaled_vars = c("wealth_index", "elevation",
-                  "LLIN_use", "time_to_district", "LLIN_wane")
-  lag_n = 3
 
-  test1 <- prep_caseData(raw_data = demo_malaria, y_var = "n_case",
-                         lagged_vars = lagged_vars, scaled_vars = scaled_vars,
-                         graph_poly = demo_polygon)
+  test_prep <- prep_caseData(raw_data = demo_malaria,
+                             y_var = "n_case",
+                             lagged_vars =  c("pev", "rain_mm", "temp_c"),
+                             scaled_vars = c("wealth_index", "elevation",
+                                             "LLIN_use", "time_to_district", "LLIN_wane"),
+                             graph_poly = demo_polygon)
 
-  # NOT FINISHED YET, WIP
+  expect_type(test_prep$W_graph, c("double"))
+  expect_s3_class(test_prep$data_prep, "data.frame")
 })
 
 test_that("seasonal imputation works", {
+  fake_df <- expand.grid(orgUnit = LETTERS[1:4],
+                         date = seq.Date(as.Date("2018-01-01"), by = "month",
+                                         length.out = 36))
+  fake_df$temp_c <- 4*sin(lubridate::month(fake_df$date))+ rnorm(nrow(fake_df))
+  fake_df$tempNA <- fake_df$temp_c
+  fake_df$tempNA[sample(1:nrow(fake_df), 0.3*nrow(fake_df))] <- NA
+  fake_df$tempNA2 <- fake_df$tempNA
 
+  fixed_df <- fill_seasonal(fake_df, data_col = "tempNA", group_col = "orgUnit")
+
+  ggplot2::ggplot(fixed_df, ggplot2::aes(x = date)) +
+    ggplot2::geom_line(ggplot2::aes(y = temp_c), col = "blue") +
+    ggplot2::geom_point(ggplot2::aes(y = tempNA2), size =2) +
+    ggplot2::geom_point(ggplot2::aes(y = tempNA), size = 1, color = "red") +
+    ggplot2::geom_line(ggplot2::aes(y = tempNA), col = "red") +
+    ggplot2::facet_wrap(~orgUnit)
+
+  expect_equal(sum(is.na(fixed_df$tempNA)), 0)
 })
